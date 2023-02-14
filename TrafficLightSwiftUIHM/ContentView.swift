64a836a4a8588009a66a3174b17b7626b4625df4
Zablotski Ilya <72109229+zablotski2020@users.noreply.github.com>
2023-02-14 18:14:17 +0300

    trafficlight

---
+++ TrafficLightSwiftUIHM/ContentView.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    //обертки вьюшек для затемненного состояния кругов световора
+    @State private var darkRed = 0.001
+    @State private var darkYellow = 0.001
+    @State private var darkGreen = 0.001
+    
+    var body: some View {
+        ZStack {
+            Color(.darkGray).ignoresSafeArea()
+            VStack {
+                ZStack {
+                    Color(.black)
+                        .frame(width: 200, height: 600)
+                        .clipShape(RoundedRectangle(cornerRadius: 70))
+                        .padding(30)
+                    VStack {
+                        TrafficConditions() // добавление красного круга
+                            .redCondition
+                            .foregroundColor(Color(red: 255, green: 0, blue: 0, opacity: darkRed))
+                        TrafficConditions() // добавление желтого круга
+                            .yellowCondition
+                            .foregroundColor(Color(red: 255, green: 255, blue: 0, opacity: darkYellow))
+                        TrafficConditions() // добавление зеленого круга
+                            .greenCondition
+                            .foregroundColor(Color(red: 0, green: 255, blue: 0, opacity: darkGreen))
+                    }
+                }
+                
+                Button(action: {
+                    changeOpacity()
+                }, label: {
+                    if darkRed == 1.0 || darkYellow == 1.0 || darkGreen == 1.0 {
+                        Text ("NEXT")
+                    } else {
+                        Text("Start")
+                    }
+                })
+                    .frame(width: 190, height: 60)
+                    .font(.system(size: 30, weight: .bold, design: .default))
+                    .foregroundColor(.white)
+                    .background(Color.black).cornerRadius(15)
+                .padding()
+            }
+        }
+    }
+    
+    private func changeOpacity(){
+        if darkRed < 1.0,
+           darkYellow < 1.0,
+           darkGreen < 1.0 {darkRed = 1.0}
+        else if darkRed == 1.0,
+                darkYellow == 0.001 {darkYellow = 1.0}
+        else if darkRed == 1.0, darkYellow == 1.0 {
+            darkRed = 0.001
+            darkYellow = 0.001
+            darkGreen = 1.0
+        } else if darkGreen == 1.0 {
+            darkGreen = 0.001
+            darkRed = 1.0
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
